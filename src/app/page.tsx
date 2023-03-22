6edfca54e5004ecff19b5abb2234c434c741a65d
Rand Seay <rand.seay@gmail.com>
2023-03-21 22:07:07 -0700

    Write a brief intro.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,22 @@ export default function Home() {
           className="rounded-full outline outline-offset-8 outline-amber-600 dark:outline-blue-300"
         />
 
-        <h1 className="text-5xl text-gray-800 dark:text-white">Hello There</h1>
+        <div className="flex flex-col gap-y-4">
+          <h1 className="font-display text-8xl">Hi 👋</h1>
+
+          <p className="text-2xl font-light">
+            I'm Rand, a Senior Frontend Engineer with{' '}
+            <a
+              className="text-blue-500 hover:underline dark:text-amber-300"
+              href="https://dispel.com/"
+            >
+              Dispel
+            </a>
+            . I was formerly with Google Cloud at both{' '}
+            <a href="https://cloud.google.com/bigquery">BigQuery</a> and{' '}
+            <a href="https://kaggle.com">Kaggle</a>.
+          </p>
+        </div>
       </div>
     </main>
   );
