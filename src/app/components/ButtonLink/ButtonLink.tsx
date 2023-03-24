8c110fdaf0620a567223c7ca87faae5d43bfc653
Rand Seay <rand.seay@gmail.com>
2023-03-23 17:20:57 -0700

    Tweak `ButtonLink` styles.

--- src/app/components/ButtonLink/ButtonLink.tsx
+++ src/app/components/ButtonLink/ButtonLink.tsx
@@ -6,7 +6,7 @@ type ButtonLinkProps = LinkProps & HTMLAttributes<HTMLAnchorElement>;
 export default function ButtonLink(props: ButtonLinkProps) {
   return (
     <Link
-      className="rounded-md bg-amber-500 py-1 px-2 text-white dark:bg-blue-500 dark:text-white"
+      className="rounded-md bg-amber-500 py-2 px-4 text-white hover:bg-amber-400 dark:bg-blue-500 dark:text-white dark:hover:bg-blue-400"
       {...props}
     />
   );
