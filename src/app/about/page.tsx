65ba5322d56a61192c6632312f449570742256c6
Rand Seay <rand.seay@gmail.com>
2023-03-26 19:20:43 -0700

    Add title to about page.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,8 @@ import PageWrap from '@components/PageWrap';
 export default function About() {
   return (
     <PageWrap>
+      <h1 className="font-display text-3xl font-medium">About</h1>
+
       <p>About</p>
     </PageWrap>
   );
