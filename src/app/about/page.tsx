331ed07bb542932032159dead91c4d5542c23ae5
Rand Seay <rand.seay@gmail.com>
2023-03-29 21:55:12 -0700

    Rewording.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ export default function About() {
       <h1 className="mb-4 font-display text-3xl font-medium">About Me</h1>
 
       <p className="text-2xl font-light">
-        I'm Rand, a Senior Frontend Engineer with{' '}
+        I'm Rand Seay, a Senior Frontend Engineer with{' '}
         <TextLink href="https://dispel.com/">Dispel</TextLink>. I was formerly
         with Google Cloud at both{' '}
         <TextLink href="https://cloud.google.com/bigquery">BigQuery</TextLink>{' '}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 
 import ButtonLink from '@components/ButtonLink';
 import PageWrap from '@components/PageWrap';
-import TextLink from '@components/TextLink';
 
 export default function Home() {
   return (
@@ -12,8 +11,8 @@ export default function Home() {
         <Image
           src="/headshot.jpg"
           alt="Headshot of Rand"
-          width={300}
-          height={300}
+          width={275}
+          height={275}
           priority
           className="rounded-full outline outline-offset-8 outline-amber-500 dark:outline-blue-300"
         />
@@ -23,14 +22,13 @@ export default function Home() {
 
           <p className="text-2xl font-light">
             My name is Rand, and I am a husband, father, and frontend engineer.
-            I love collaborating with talented people in the space where
-            engineering meets design.
+            I love collaborating in the space where engineering meets design.
           </p>
 
           <div className="inline-flex">
             <ButtonLink href="/about">
               <span className="flex items-center gap-x-1">
-                More <ArrowRightIcon className="h-5 w-5" />
+                About <ArrowRightIcon className="h-5 w-5" />
               </span>
             </ButtonLink>
           </div>
