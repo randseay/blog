0c3979c1cbee41688479a17b52c69b9973fa4012
Rand Seay <rand.seay@gmail.com>
2023-06-05 21:43:05 -0700

    Make the logo into a home link.

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,23 +1,24 @@
 import ButtonLink from '@components/ButtonLink';
+import Link from 'next/link';
+
 import Logo from '@components/Logo';
 import ThemeSwitch from '@components/ThemeSwitch';
 
 export default function Header() {
   return (
     <div className="flex items-center justify-between p-4">
-      <div className="flex items-center gap-x-2">
-        <div className="h-10 w-10 text-blue-400 dark:text-amber-300">
-          <Logo />
-        </div>
+      <Link href="/">
+        <div className="flex items-center gap-x-2">
+          <div className="h-10 w-10 text-blue-400 dark:text-amber-300">
+            <Logo />
+          </div>
 
-        <h1 className="font-display text-2xl font-light">Rand Seay</h1>
-      </div>
+          <h1 className="font-display text-2xl font-light">Rand Seay</h1>
+        </div>
+      </Link>
 
       <div className="flex items-center gap-x-6">
         <nav className="flex items-center gap-x-2">
-          <ButtonLink variant="secondary" href="/">
-            Home
-          </ButtonLink>
           <ButtonLink variant="secondary" href="/about">
             About
           </ButtonLink>
