cda3657c011d1bcba7e849fcd37bf2b46fe07eeb
Rand Seay <rand.seay@gmail.com>
2023-06-05 21:09:58 -0700

    Tweak the about page.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,9 +4,9 @@ import TextLink from '@components/TextLink';
 export default function About() {
   return (
     <PageWrap>
-      <h1 className="mb-4 font-display text-3xl font-medium">About Me</h1>
+      <h1 className="mb-4 font-display text-3xl font-medium">About me</h1>
 
-      <p className="text-2xl font-light">
+      <p className="mb-4 text-2xl font-light">
         I'm Rand Seay, a Senior Frontend Engineer with{' '}
         <TextLink href="https://dispel.com/">Dispel</TextLink>. I was formerly
         with Google Cloud at both{' '}
@@ -14,11 +14,17 @@ export default function About() {
         and <TextLink href="https://kaggle.com">Kaggle</TextLink>.
       </p>
 
-      <p>
+      <p className="mb-8 text-2xl font-light">
         My wife and I and our three kids are in the greater Portland area, and
         love the Pacific Northwest. I enjoy woodworking, cooking, and playing
         basketball.
       </p>
+
+      <h1 className="mb-4 font-display text-3xl font-medium">
+        How I got started
+      </h1>
+
+      <p className="mb-4 text-2xl font-light"></p>
     </PageWrap>
   );
 }
