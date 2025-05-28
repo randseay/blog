f55c3cb6839bf7a1b4bd62ab8b814084d79c04e9
Rand Seay <rand.seay@gmail.com>
2025-05-27 19:27:05 -0700

    Make `Tag` into a link when not on the `/articles` page.

--- src/app/components/Tag/Tag.tsx
+++ src/app/components/Tag/Tag.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import classNames from 'classnames';
-import { useRouter, useSearchParams } from 'next/navigation';
+import Link from 'next/link';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 type TagProps = {
   tag: string;
@@ -12,6 +13,8 @@ type TagProps = {
 export default function Tag({ tag, variant = 'small', selected }: TagProps) {
   const router = useRouter();
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const isArticlesPage = pathname === '/articles';
 
   // Helper to update the URL
   const setTag = (tag: string | null) => {
@@ -24,19 +27,34 @@ export default function Tag({ tag, variant = 'small', selected }: TagProps) {
     router.replace(`?${params.toString()}`, { scroll: false });
   };
 
+  const tagClassNames = classNames(
+    'rounded-full px-3 py-1 font-semibold inline-block',
+    {
+      'text-xl': variant === 'large',
+      'text-base': variant === 'small',
+      'bg-amber-500 text-white dark:bg-blue-500': selected,
+      'bg-gray-200 text-gray-600 hover:bg-gray-300 hover:text-gray-800 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200':
+        !selected,
+    }
+  );
+
+  if (isArticlesPage) {
+    return (
+      <button
+        role="button"
+        className={tagClassNames}
+        onClick={() => setTag(tag === 'all' ? null : tag)}
+      >
+        {tag}
+      </button>
+    );
+  }
+
   return (
-    <button
-      role="button"
-      className={classNames('rounded-full px-3 py-1 font-semibold', {
-        'text-xl': variant === 'large',
-        'text-base': variant === 'small',
-        'bg-amber-500 text-white dark:bg-blue-500': selected,
-        'bg-gray-200 text-gray-600 hover:bg-gray-300 hover:text-gray-800 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200':
-          !selected,
-      })}
-      onClick={() => setTag(tag === 'all' ? null : tag)}
-    >
-      {tag}
-    </button>
+    <span className="not-prose">
+      <Link href={`/articles?tag=${tag}`} className={tagClassNames}>
+        {tag}
+      </Link>
+    </span>
   );
 }
