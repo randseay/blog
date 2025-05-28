1ba4b60aeea5b611c43f9813cff918dba5412b76
Rand Seay <rand.seay@gmail.com>
2025-05-27 19:34:05 -0700

    Fix date formatting.

--- src/app/components/DateTimeStamp/DateTimeStamp.tsx
+++ src/app/components/DateTimeStamp/DateTimeStamp.tsx
@@ -1,11 +1,14 @@
 import { CalendarIcon } from '@heroicons/react/24/outline';
 
 export function DateTimeStamp({ date }: { date: string }) {
-  const timestamp = new Date(date).toLocaleDateString('en-US', {
+  const [year, month, day] = date.split('-').map(Number);
+  const localDate = new Date(year, month - 1, day); // month is 0-based
+  const timestamp = localDate.toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
   });
+
   return (
     <div className="flex items-center gap-x-2">
       <CalendarIcon className="h-5 w-5 stroke-2" />
