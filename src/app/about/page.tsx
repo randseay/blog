f1f21dfc57ecbc8d9619b3e7ddab6ecde7557552
Rand Seay <rand.seay@gmail.com>
2025-12-11 21:34:49 -0800

    Add back link to the about page.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,20 @@
+import ButtonLink from '@components/ButtonLink';
 import PageWrap from '@components/PageWrap';
 import Prose from '@components/Prose';
+import { ArrowLeftIcon } from '@heroicons/react/24/outline';
 
 import Content from './content.mdx';
 
 export default function About() {
   return (
     <PageWrap>
-      <Prose>
+      <div className="flex mb-16">
+        <ButtonLink href="/" variant="secondary" className="gap-2">
+          <ArrowLeftIcon className="h-5 w-5 stroke-2" />
+          Back Home
+        </ButtonLink>
+      </div>
+      <Prose className="px-4">
         <Content />
       </Prose>
     </PageWrap>
